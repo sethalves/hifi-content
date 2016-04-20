6913abe5fe4ef9eb8a1639216ae8f81b74e30658
Seth Alves <seth.alves@gmail.com>
2016-04-20 13:57:22 -0700

    make some changes suggested by howard

--- baton/baton-client.js
+++ baton/baton-client.js
@@ -1,59 +1,61 @@
 
 
-runByOne = function(batonName, maxTime, grantedThunk, deniedThunk) {
+runByOne = function(participant, batonName, maxTime, grantedThunk, deniedThunk) {
     var react;
-
-    print("runByOne");
 
     react = function(channel, message, sender) {
         if (channel != "baton") {
             return;
         }
 
-        var messageParsed = message.split(" ");
-        if (messageParsed.length < 2) {
-            print("invalid message: " + message);
+        var messageParsed;
+        try {
+            messageParsed = JSON.parse(message);
+        } catch (e) {
+            print("unparsable message: " + message);
             return;
         }
 
-        var command = messageParsed[0];
-        var messageBatonName = messageParsed[1];
+        var command = messageParsed.command;
+        var messageBatonName = messageParsed.name;
+        var messageParticipant = messageParsed.participant;
 
+        if (!command || !messageBatonName || !messageParticipant) {
+            print("invalid message: " + message);
+            return;
+        }
         if (batonName != messageBatonName) {
+            return;
+        }
+        if (participant != messageParticipant) {
             return;
         }
 
         if (command == "grant") {
-            if (messageParsed.length != 3) {
-                print("invalid message: " + message);
-                return;
-            }
-
-            var grantedTo = messageParsed[2];
-            if (grantedTo == MyAvatar.sessionUUID) {
-                print("GRANTED " + batonName);
-                Messages.messageReceived.disconnect(react);
-                grantedThunk();
-            }
+            print("GRANTED " + batonName);
+            Messages.messageReceived.disconnect(react);
+            grantedThunk();
         } else if (command == "deny") {
-            if (messageParsed.length != 3) {
-                print("invalid message: " + message);
-                return;
-            }
-            var deniedFrom = messageParsed[2];
-            if (deniedFrom == MyAvatar.sessionUUID) {
-                print("DENIED " + batonName);
-                Messages.messageReceived.disconnect(react);
-                deniedThunk();
-            }
+            print("DENIED " + batonName);
+            Messages.messageReceived.disconnect(react);
+            deniedThunk();
         }
     }
 
     Messages.messageReceived.connect(react);
     Messages.subscribe("baton");
-    Messages.sendMessage("baton", "claim " + batonName + " " + maxTime);
+    Messages.sendMessage("baton", JSON.stringify({
+        command: "claim",
+        name: batonName,
+        participant: participant,
+        time: maxTime
+    }));
 }
 
-releaseBaton = function(batonName) {
-    Messages.sendMessage("baton", "release " + batonName);
+releaseBaton = function(participant, batonName) {
+    Messages.sendMessage("baton", JSON.stringify({
+        command: "release",
+        name: batonName,
+        participant: participant
+    }));
 }
--- baton/baton-server.js
+++ baton/baton-server.js
@@ -3,89 +3,116 @@
 //
 
 
-var NULL_ID = "{00000000-0000-0000-000000000000}";
-
-var owners = {};
-var timeouts = {};
-
+var batons = {}; // hash key is baton-name, value is [owner, timeout] -- owner is a participant, timeout is msecs
 
 Messages.subscribe("baton");
 Messages.messageReceived.connect(function(channel, message, sender) {
     var _this = this;
-    // this.owners = {};
-    // this.timeouts = {};
 
-    this.grant = function (sender, maxTime, batonName) {
-        print("GRANT: '" + batonName + "'");
-
-        if (!(batonName in owners)) {
-            // a baton name we haven't seen before
-            owners[batonName] = NULL_ID;
-            timeouts[batonName] = null;
+    this.batonOwner = function(baton) {
+        if (baton) {
+            return baton[0];
         }
+        return null;
+    }
 
-        if (owners[batonName] == NULL_ID || owners[batonName] == sender) {
+    this.batonTimeout = function(baton) {
+        if (baton) {
+            return baton[1];
+        }
+        return null;
+    }
+
+    this.setBaton = function(batonName, owner, timeout) {
+        batons[batonName] = [owner, timeout];
+    }
+
+    this.grant = function (participant, maxTime, batonName) {
+        var baton = batons[batonName];
+        var owner = _this.batonOwner(baton);
+        if (!owner || owner == participant) {
+            print("GRANT: '" + batonName + "' to " + participant);
             // the baton will be granted (or regranted)
-            owners[batonName] = sender;
+            owner = participant;
             // if there's an old timeout, clear it
-            if (timeouts[batonName]) {
-                Script.clearTimeout(timeouts[batonName]);
+            timeout = _this.batonTimeout(baton);
+            if (timeout) {
+                Script.clearTimeout(timeout);
             }
             // set new timeout
-            timeouts[batonName] = Script.setTimeout(function() {
+            timeout = Script.setTimeout(function() {
                 print("TIMEOUT: '" + batonName + "'");
-                if (owners[batonName] != NULL_ID) {
-                    Messages.sendMessage("baton", "timeout " + batonName + " " + owners[batonName]);
-                    owners[batonName] = NULL_ID;
+                if (batonName in batons) {
+                    delete batons[batonName];
+                    Messages.sendMessage("baton", JSON.stringify({
+                        command: "timeout",
+                        name: batonName,
+                        participant: participant
+                    }));
                 }
             }, maxTime);
-            Messages.sendMessage("baton", "grant " + batonName + " " + owners[batonName]);
+            _this.setBaton(batonName, participant, timeout);
+            Messages.sendMessage("baton", JSON.stringify({
+                command: "grant",
+                name: batonName,
+                participant: participant
+            }));
         } else {
-            Messages.sendMessage("baton", "deny " + batonName + " " + owners[batonName]);
+            print("DENY: '" + batonName + "' from " + participant);
+            Messages.sendMessage("baton", JSON.stringify({
+                command: "deny",
+                name: batonName,
+                participant: participant
+            }));
         }
     };
 
-    this.release = function (sender, batonName) {
-        print("RELEASE: '" + batonName + "'");
-        if (!(batonName in owners)) {
-            print(sender + " tried to release unknown baton " + batonName);
+    this.release = function (participant, batonName) {
+        var baton = batons[batonName];
+        if (!baton) {
+            print(participant + " tried to release unknown baton " + batonName);
             return;
         }
-        if (owners[batonName] != sender) {
-            print(sender + " tried to release baton " + batonName + ", which is owned by " + owners[batonName]);
+        if (_this.batonOwner(baton) != participant) {
+            print(participant + " tried to release baton " + batonName + ", which is owned by " + _this.batonOwner(baton));
             return;
         }
-        if (timeouts[batonName]) {
-            print("clearing timeout");
-            Script.clearTimeout(timeouts[batonName]);
-        }
-        owners[batonName] = NULL_ID;
-        timeouts[batonName] = null;
-        Messages.sendMessage("baton", "released " + batonName + " " + owners[batonName]);
+        print("RELEASE: '" + batonName + "' by " + participant);
+        Script.clearTimeout(_this.batonTimeout(baton));
+        delete batons[batonName];
+        Messages.sendMessage("baton", JSON.stringify({
+            command: "released",
+            name: batonName,
+            participant: participant
+        }));
     };
 
     // main body of message receiver
 
-    var messageParsed = message.split(" ");
-    if (messageParsed.length < 2) {
+    var messageParsed;
+    try {
+        messageParsed = JSON.parse(message);
+    } catch (e) {
+        print("unparsable message: " + message);
+        return;
+    }
+    var participant = messageParsed.participant;
+    var command = messageParsed.command;
+    var batonName = messageParsed.name;
+
+    if (!participant || !command || !batonName) {
         print("invalid message: " + message);
         return;
     }
-    var command = messageParsed[0];
-    var batonName = messageParsed[1];
 
     if (command == "claim") {
-        if (messageParsed.length != 3) {
+        var maxTime = messageParsed.time;
+        if (!maxTime) {
             print("invalid claim message: " + message);
             return;
         }
-        var maxTime = messageParsed[2];
-        _this.grant(sender, maxTime, batonName);
+        this.grant(participant, maxTime, batonName);
     } else if (command == "release") {
-        if (messageParsed.length != 2) {
-            print("invalid release message: " + message);
-            return;
-        }
-        _this.release(sender, batonName);
+        this.release(participant, batonName);
     }
 });
