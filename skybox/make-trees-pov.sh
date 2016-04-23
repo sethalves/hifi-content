16066755cc99aab1bc3ef1e95e7ba7bafe6a4978
Seth Alves <seth.alves@gmail.com>
2016-04-22 21:17:33 -0700

    change how trees are placed

--- skybox/make-trees-pov.sh
+++ skybox/make-trees-pov.sh
@@ -4,60 +4,19 @@ L_SYSTEM_TREE=../L-system-tree/L-system-tree
 
 cat skybox-header.pov > trees.pov
 
-# ${L_SYSTEM_TREE} -p 0 0 100 -v -w 5 -t iixxyiO -o tree.pov
-# cat tree.pov >> trees.pov
+pi=`echo "4*a(1)" | bc -l`
 
-# ${L_SYSTEM_TREE} -p 20 0 100 -v -w 5 -t iixxyiO -o tree.pov
-# cat tree.pov >> trees.pov
-
-
-for ((i=1;i<=200;i++));
+for ((i=1;i<=300;i++));
 do
-    X=$(( ( RANDOM % 150 )  + -100 ))
+    A=$(( ( RANDOM % 360 ) ))
+    rad=`echo "$A*($pi/180)" | bc -l`
+
+    D=$(( ( RANDOM % 500 )  + 40))
+
+    Z=`echo "$D*s($rad)" | bc -l`
+    X=`echo "$D*c($rad)" | bc -l`
+
     Y=0
-    Z=$(( ( RANDOM % 50 )  + 50 ))
-    R=$(( ( RANDOM % 360 ) ))
-    W=$(( ( RANDOM % 3 )  + 3))
-
-    ${L_SYSTEM_TREE} -p $X $Y $Z -r 0 $R 0 -v -w $W -t iixxyiO -o tree.pov
-    cat tree.pov >> trees.pov
-    rm tree.pov
-done
-
-
-for ((i=1;i<=200;i++));
-do
-    X=$(( ( RANDOM % 150 )  + -50 ))
-    Y=0
-    Z=$(( ( RANDOM % 50 )  + -100 ))
-    R=$(( ( RANDOM % 360 ) ))
-    W=$(( ( RANDOM % 3 )  + 3))
-
-    ${L_SYSTEM_TREE} -p $X $Y $Z -r 0 $R 0 -v -w $W -t iixxyiO -o tree.pov
-    cat tree.pov >> trees.pov
-    rm tree.pov
-done
-
-
-for ((i=1;i<=200;i++));
-do
-    X=$(( ( RANDOM % 50 )  + -100 ))
-    Y=0
-    Z=$(( ( RANDOM % 150 )  + -100 ))
-    R=$(( ( RANDOM % 360 ) ))
-    W=$(( ( RANDOM % 3 )  + 3))
-
-    ${L_SYSTEM_TREE} -p $X $Y $Z -r 0 $R 0 -v -w $W -t iixxyiO -o tree.pov
-    cat tree.pov >> trees.pov
-    rm tree.pov
-done
-
-
-for ((i=1;i<=200;i++));
-do
-    X=$(( ( RANDOM % 50 )  + 50 ))
-    Y=0
-    Z=$(( ( RANDOM % 150 )  + -50 ))
     R=$(( ( RANDOM % 360 ) ))
     W=$(( ( RANDOM % 3 )  + 3))
 
