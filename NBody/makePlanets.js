0092d13a9e1d933c7e5920e7ee0cb7c838bd0601
Seth Alves <seth.alves@gmail.com>
2016-04-28 14:53:00 -0700

    moon leaves a particle trail

--- NBody/makePlanets.js
+++ NBody/makePlanets.js
@@ -63,7 +63,9 @@ function onButtonClick() {
         modelURL: "http://headache.hungry.com/~seth/hifi/NBody/earth.fbx",
         position: inFrontOfMe(2 * EARTH_SIZE),
         dimensions: { x: EARTH_SIZE, y: EARTH_SIZE, z: EARTH_SIZE },
-        shapeType: "sphere"
+        shapeType: "sphere",
+        angularDamping: 0,
+        angularVelocity: { x: 0, y: 0.1, z: 0 },
     });
     moon = Entities.addEntity({
         type: "Model",
@@ -76,6 +78,64 @@ function onButtonClick() {
         angularDamping: 0, // 0.01,
         script: SCRIPT_URL,
         shapeType: "sphere"
+    });
+    Entities.addEntity({
+        "accelerationSpread": {
+            "x": 0,
+            "y": 0,
+            "z": 0
+        },
+        "alpha": 1,
+        "alphaFinish": 0,
+        "alphaStart": 1,
+        "azimuthFinish": 0,
+        "azimuthStart": 0,
+        "color": {
+            "blue": 255,
+            "green": 255,
+            "red": 255
+        },
+        "colorFinish": {
+            "blue": 255,
+            "green": 255,
+            "red": 255
+        },
+        "colorStart": {
+            "blue": 255,
+            "green": 255,
+            "red": 255
+        },
+        "dimensions": {
+            "x": 0.10890001058578491,
+            "y": 0.10890001058578491,
+            "z": 0.10890001058578491
+        },
+        "emitAcceleration": {
+            "x": 0,
+            "y": 0,
+            "z": 0
+        },
+        "emitOrientation": {
+            "w": 0.99999994039535522,
+            "x": 0,
+            "y": 0,
+            "z": 0
+        },
+        "emitRate": 300,
+        "emitSpeed": 0,
+        "emitterShouldTrail": 1,
+        "maxParticles": 10000,
+        "name": "moon trail",
+        "parentID": moon,
+        "particleRadius": 0.005,
+        "radiusFinish": 0.005,
+        "radiusSpread": 0.005,
+        "radiusStart": 0.005,
+        "speedSpread": 0,
+        "lifespan": 20,
+        "textures": "https://hifi-public.s3.amazonaws.com/alan/Particles/Particle-Sprite-Smoke-1.png",
+        "type": "ParticleEffect",
+        "userData": "{\"grabbableKey\":{\"grabbable\":false}}"
     });
 }
 
