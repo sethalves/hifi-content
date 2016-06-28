1af296f870aafa9d26aa9fe0f0e5a05f0f785908
Seth Alves <seth.alves@gmail.com>
2016-06-28 16:38:06 -0700

    raygun can shoot avatars, now

--- raygun/raygun.js
+++ raygun/raygun.js
@@ -8,8 +8,33 @@
     rayGun = genericTool(
         // start
         function() {
-            var props = Entities.getEntityProperties(this.getEntityID());
-            this.createRay();
+            var origin = this.pickRay.origin;
+            var direction = Vec3.normalize(this.pickRay.direction);
+            var distance = -1;
+            var hitPoint;
+            if (this.targetAvatar) {
+                hitPoint = Vec3.sum(origin, Vec3.multiply(this.avatarDistance, direction));
+                distance = this.avatarDistance;
+            }
+            if (this.targetEntity && (distance < 0 || this.entityDistance < distance)) {
+                hitPoint = Vec3.sum(origin, Vec3.multiply(this.entityDistance, direction));
+                distance = this.entityDistance;
+            }
+
+            if (distance > 0) {
+                Entities.addEntity({
+                    type: "Sphere",
+                    position: hitPoint,
+                    color: {red: 0, green: 200, blue: 0},
+                    dimensions: 0.2,
+                    lifetime: 1.0
+                });
+            } else {
+                distance = 20;
+            }
+
+
+            this.createRay(distance);
         },
         // continue
         function() {
@@ -21,7 +46,7 @@
         }
     );
 
-    rayGun.createRay = function() {
+    rayGun.createRay = function(distance) {
         this.rayCreatedTime = Date.now();
         this.rayID = Entities.addEntity({
             name: "raygun beam",
@@ -30,13 +55,13 @@
             collidesWith: "",
             collisionless: true,
             registrationPoint: { x: 0.5, y: 0.5, z: 0 },
-            dimensions: { x: 0.01, y: 0.01, z: 20 },
+            dimensions: { x: 0.01, y: 0.01, z: distance },
             color: { red: 0, green: 255, blue: 0 },
             parentID: this.getEntityID(),
             parentJointIndex: -1,
             localRotation: {x: 0, y: 0, z: 0, w: 1},
             localPosition: {x: 0, y: 0.008, z: 0.12},
-            lifetime: 0.1
+            lifetime: 0.08
         });
     };
 
