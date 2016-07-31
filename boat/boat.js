abb9ca587c99570aaf4f66b160c2deab67b1912f
Seth Alves <seth.alves@gmail.com>
2016-07-30 17:34:46 -0700

    script to move something back and forth

---
+++ boat/boat.js
@@ -0,0 +1,69 @@
+
+(function() {
+    Script.include("http://headache.hungry.com/~seth/hifi/baton-client.js");
+
+    Boat = (function() {
+        var _this = this;
+
+        this.batonName = null;
+        this.baton = null;
+
+        this.moveBoat = function() {
+            var boatProperties = Entities.getEntityProperties(this.boatID, ["position", "velocity"]);
+            var boatPosition = boatProperties.position;
+            var boatVelocity = boatProperties.velocity;
+            var newVelocity = {x: 0, y: 0, z: 0};
+            var velocityNeedsUpdate = false;
+
+            if (boatPosition.z < 65.0 && boatVelocity.z < 0.0) {
+                newVelocity.z = 1.0;
+                velocityNeedsUpdate = true;
+            } else if (boatPosition.z > 85.0 && boatVelocity.z > 0.0) {
+                newVelocity.z = -1.0;
+                velocityNeedsUpdate = true;
+            } else if (Vec3.length(boatVelocity) < 0.5) {
+                newVelocity.z = 1.0;
+                velocityNeedsUpdate = true;
+            }
+
+            if (velocityNeedsUpdate) {
+                _this.baton.claim(
+                    function () { // onGrant
+                        Entities.editEntity(_this.boatID, {velocity: newVelocity});
+                        _this.baton.release();
+                    },
+                    null, // onRelease
+                    null, // onDenied
+                    function () { // onNoServerResponse
+                        print("no response from baton-server");
+                    }
+                );
+            }
+        };
+
+        this.preload = function(entityID) {
+            this.boatID = entityID;
+
+            this.maintenanceInterval = Script.setInterval(function() {
+                _this.moveBoat();
+            }, 1000);
+
+            this.batonName = 'io.highfidelity.seth.Boat:' + this.boatID;
+            this.baton = acBaton({
+                batonName: this.batonName,
+                timeScale: 15000
+            });
+
+            this.moveBoat();
+        };
+    });
+
+    Boat.prototype = {
+        handleMessage: function(id, params) {
+            boat.handleMessages(id, params);
+        }
+    }
+
+    boat = new Boat();
+    return boat;
+});
