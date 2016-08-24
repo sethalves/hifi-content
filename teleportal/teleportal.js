ac0cc2410d9cb0c29acf8c60b65ba7b3f2b3aee5
Seth Alves <seth.alves@gmail.com>
2016-08-23 17:54:42 -0700

    jslint

--- teleportal/teleportal.js
+++ teleportal/teleportal.js
@@ -1,13 +1,15 @@
+/* global MyAvatar, Vec3, Script, getEntityCustomData */
 (function(){
+    Script.include("http://headache.hungry.com/~seth/hifi/utils.js");
     this.teleportAvatar = function(teleportalZoneID) {
         var destination = getEntityCustomData('teleportal-destination', teleportalZoneID, null);
         if (destination) {
             Window.location = destination;
             MyAvatar.addThrust(Vec3.multiply(MyAvatar.velocity, -1.0)); // negate velocity
         }
-    }
+    };
 
     this.enterEntity = function(teleportalZoneID) {
         this.teleportAvatar(teleportalZoneID);
-    }
+    };
 })
