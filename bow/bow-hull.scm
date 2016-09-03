87daa1ae29234569cc21ae0929ffdc3887da8027
Seth Alves <seth.alves@gmail.com>
2016-09-03 13:10:41 -0700

    smaller collision hull

--- bow/bow-hull.scm
+++ bow/bow-hull.scm
@@ -11,7 +11,7 @@
 (use-modules (ao shapes) (ao transforms) (ao csg) (ao export))
 
 ;; (define bbox (cube '(-0.020986 -0.675867 -0.053568) '(0.020237 0.630563 0.162057)))
-(define bbox (cube '(-0.020986 -0.675867 -0.053568) '(0.020237 0.630563 0.162057)))
+(define bbox (cube '(-0.023 -0.40 -0.06) '(0.023 0.40 0.08)))
 ;; '(-1 -1 -1) '(1 1 1)
 ;; (ao-show bbox)
-(ao-export-mesh bbox "bow_collision_hull.obj" '(-0.020986 -0.675867 -0.053568) '(0.020237 0.630563 0.162057) 2)
+(ao-export-mesh bbox "bow_collision_hull.obj" '(-0.023 -0.675867 -0.053568) '(0.023 0.630563 0.182057) 2)
