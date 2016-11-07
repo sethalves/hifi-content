94ad95ed4c2ad303a4f24658b85b3f7ff4b7d6c9
Seth Alves <seth.alves@gmail.com>
2016-11-07 15:57:03 -0800

    sync

---
+++ voxel-paint/voxel-paint-brush.js
@@ -0,0 +1,23 @@
+
+/* global Entities, genericTool, Script */
+
+(function() {
+    Script.include("http://headache.hungry.com/~seth/hifi/hcEdit/genericTool.js");
+
+    return genericTool(
+        function() { // start
+            this.brush = Entities.getChildrenIDs(this.entityID)[0];
+        },
+        function() { // continue
+            var brushProps = Entities.getEntityProperties(this.brush, ["position", "rotation",
+                                                                        "dimensions", "registrationPoint"]);
+            var searchRadius = 2.0;
+            var editSphereRadius = 0.4;
+
+            var ids = Entities.findEntities(brushProps.position, searchRadius);
+            for (var i = 0; i < ids.length; i++) {
+                Entities.setVoxelSphere(ids[i], brushProps.position, editSphereRadius, 255);
+            }
+        },
+        null); // stop
+});
