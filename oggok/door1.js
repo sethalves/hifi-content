717d97d16ac46fe8a1184ab67e4a266097218330
Seth Alves <seth.alves@gmail.com>
2017-08-27 20:18:15 -0700

    all 4 doors work

--- oggok/door1.js
+++ oggok/door1.js
@@ -11,10 +11,10 @@
         var rotation = Entities.getEntityProperties(this.entityID, ["rotation"]).rotation;
         var rotEulers = Quat.safeEulerAngles(rotation); // degrees
 
-        if (rotEulers.y > 5) {
+        if (rotEulers.y < 175 && rotEulers.y > -175) {
             // it's open, so close it
             Entities.addAction("tractor", this.entityID, {
-                targetRotation: Quat.fromPitchYawRollDegrees(0, 0, 0),
+                targetRotation: Quat.fromPitchYawRollDegrees(0, 180, 0),
                 angularTimeScale: 0.5,
                 tag: "oggok close door",
                 ttl: 2.5
@@ -22,7 +22,7 @@
         } else {
             // it's closed, so open it
             Entities.addAction("tractor", this.entityID, {
-                targetRotation: Quat.fromPitchYawRollDegrees(0, 90, 0),
+                targetRotation: Quat.fromPitchYawRollDegrees(0, -90, 0),
                 angularTimeScale: 0.5,
                 tag: "oggok open door",
                 ttl: 2.5
@@ -30,30 +30,10 @@
         }
     };
 
-    this.turnOff = function () {
-    };
-
-    // this.startNearTrigger = function (entityID) {
-    //     this.entityID = entityID;
-    //     this.turnOn();
-    // };
-
-    // this.stopNearTrigger = function (entityID) {
-    //     this.entityID = entityID;
-    //     this.turnOff();
-    // };
-
     this.clickDownOnEntity = function (entityID, mouseEvent) {
         if (mouseEvent.isPrimaryButton) {
             this.entityID = entityID;
             this.turnOn();
-        }
-    };
-
-    this.clickReleaseOnEntity = function (entityID, mouseEvent) {
-        if (mouseEvent.isPrimaryButton) {
-            this.entityID = entityID;
-            this.turnOff();
         }
     };
 });
