c407a16c82bc6421221fc14707956fe52634f79a
Seth Alves <seth.alves@gmail.com>
2018-01-07 09:23:13 -0800

    windows

--- portal/override.js
+++ portal/override.js
@@ -1,4 +1,5 @@
 var publicPath = "http://headache.hungry.com/~seth/hifi/portal/";
-var basePath = "file:///home/seth/src/hifi-content/portal/";
+// var basePath = "file:///home/seth/src/hifi-content/portal/";
+var basePath = "file:///c:/Users/alves/src/hifi-content/portal/";
 print("Base path " + basePath);
 Resources.overrideUrlPrefix(publicPath, basePath);
