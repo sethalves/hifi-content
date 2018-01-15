ff5639f20da1ee788ece7ce9631d9c165e97a7a3
Seth Alves <seth.alves@gmail.com>
2018-01-15 11:01:58 -0800

    sync

--- domain-cleaner/domain-cleaner.js
+++ domain-cleaner/domain-cleaner.js
@@ -1,12 +1,10 @@
-
 "use strict";
 
-/* global Script, Tablet */
+/* global Entities, Script, Tablet, Assets, MyAvatar, Vec3 */
 
 (function() { // BEGIN LOCAL_SCOPE
 
-    var DOMAIN_CLEANER_UI_URL = Script.resolvePath("domain-cleaner.html");
-    var EVENT_KEY = "domain-cleaner-command";
+    var DOMAIN_CLEANER_URL = Script.resolvePath("domain-cleaner.html");
 
     var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
 
@@ -17,19 +15,77 @@
     });
 
     function saveDomain(params) {
-        print("SAVE");
+        var entitiesToSave = [];
+        var entityIDs = Entities.findEntities(MyAvatar.position, 1000);
+        for (var j = 0; j < entityIDs.length; j++) {
+            var entityID = entityIDs[j];
+            var props = Entities.getEntityProperties(entityID);
+            if (props.locked) {
+                continue;
+            }
+            delete props.clientOnly;
+            delete props.created;
+            delete props.lastEdited;
+            delete props.lastEditedBy;
+            delete props.owningAvatarID;
+            delete props.queryAACube;
+            delete props.age;
+            delete props.ageAsText;
+            delete props.naturalDimensions;
+            delete props.naturalPosition;
+            delete props.acceleration;
+            delete props.scriptTimestamp;
+            delete props.boundingBox;
+            delete props.localPosition;
+            delete props.localRotation;
+            delete props.localVelocity;
+            delete props.localAngularVelocity;
+            delete props.localDimensions;
+            delete props.renderInfo;
+
+            entitiesToSave.push(props);
+        }
+
+        var data = JSON.stringify(entitiesToSave);
+        Assets.uploadData(data, function(url, hash) {
+            Assets.setMapping("/domain-cleaner-data.json", hash, function() {
+            });
+        });
     }
 
     function restoreDomain(params) {
-        print("RESTORE");
+        Assets.getMapping("/domain-cleaner-data.json", function (hash) {
+            Assets.downloadData(hash, function (data) {
+                print("done downloading...");
+                var savedEntityProps = JSON.parse(data);
+                print("done parsing...");
+                for (var i; i < savedEntityProps.length; i++) {
+                    var props = savedEntityProps[i];
+                    var entityID = props.id;
+                    delete props.id;
+                    Entities.editEntity(entityID, props);
+                }
+            });
+        });
     }
 
     function deleteUnknown(params) {
-        print("DELETE");
+        var pos = Vec3.sum(MyAvatar.position, Vec3.multiplyQbyV(MyAvatar.orientation, {x: 0, y: 0.1, z: -2}));
+        Entities.addEntity({
+            name: "hinge test ",
+            type: "Box",
+            color: { blue: 200, green: 0, red: 0 },
+            dimensions: { x: 0.2, y: 0.2, z: 0.1 },
+            position: Vec3.sum(pos, {x: 0, y: 1, z:0}),
+            dynamic: false,
+            collisionless: true,
+            lifetime: 120,
+            gravity: { x: 0, y: 0, z: 0 }
+        });
     }
 
     function onWebEventReceived(eventString) {
-        print("domain-cleaner received web event: " + JSON.stringify(eventString));
+        print("received web event: " + JSON.stringify(eventString));
         if (typeof eventString === "string") {
             var event;
             try {
@@ -38,14 +94,14 @@
                 return;
             }
 
-            if (event[EVENT_KEY]) {
+            if (event["domain-cleaner-command"]) {
                 var commandToFunctionMap = {
-                    "save": saveDomain,
-                    "restore": restoreDomain,
-                    "delete": deleteUnknown
+                    "save-domain": saveDomain,
+                    "restore-domain": restoreDomain,
+                    "delete-unknown": deleteUnknown
                 };
 
-                var cmd = event[EVENT_KEY];
+                var cmd = event["domain-cleaner-command"];
                 if (commandToFunctionMap.hasOwnProperty(cmd)) {
                     var func = commandToFunctionMap[cmd];
                     func(event);
@@ -54,24 +110,24 @@
         }
     }
 
-    var onCleanupScreen = false;
+    var onDomainCleanerScreen = false;
     var shouldActivateButton = false;
 
     function onClicked() {
-        if (onCleanupScreen) {
+        if (onDomainCleanerScreen) {
             tablet.gotoHomeScreen();
         } else {
             shouldActivateButton = true;
-            tablet.gotoWebScreen(DOMAIN_CLEANER_UI_URL);
-            onCleanupScreen = true;
+            tablet.gotoWebScreen(DOMAIN_CLEANER_URL);
+            onDomainCleanerScreen = true;
         }
     }
 
     function onScreenChanged() {
         // for toolbar mode: change button to active when window is first openend, false otherwise.
         button.editProperties({isActive: shouldActivateButton});
+        onDomainCleanerScreen = shouldActivateButton;
         shouldActivateButton = false;
-        onCleanupScreen = shouldActivateButton;
     }
 
     function cleanup() {
