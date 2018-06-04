af9657b6b191323bea808b28ade5a15d3bb44c70
Seth Alves <seth.alves@gmail.com>
2018-06-03 17:15:08 -0700

    more planets, json output

--- orrery/orrery-web-api.cpp
+++ orrery/orrery-web-api.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 using namespace std;
 
+#include <string.h>
+#include <ctime>
+
 extern "C" {
 #include "cspice/include/SpiceCK.h"
 #include "cspice/include/SpiceZpr.h"
@@ -10,30 +13,41 @@ extern "C" {
 
 int main (int argc, char *argv[]) {
 
-    // furnsh_c("data/earth_assoc_itrf93.tf");
-    // furnsh_c("data/naif0012.tls");
-    // furnsh_c("data/de438.bsp");
-
-    // furnsh_c("data/earth_assoc_itrf93.tf");
-    // furnsh_c("data/EARTH_000101_070620_070329.BPC");
     furnsh_c("data/pck00011.tpc");
     furnsh_c("data/naif0012.tls");
     furnsh_c("data/de438.bsp");
 
-    const char* utc { "Sun Jun  3 15:26:35 UTC 2018" };
+    time_t now = time(0);
+    tm *gmtm = gmtime(&now);
+    char utc[128];
+    strftime(utc, sizeof(utc), "%a %b %d %H:%M:%S UTC %Y", gmtm);
+
     SpiceDouble et;
     utc2et_c(utc, &et);
 
     SpiceDouble ptarg[3];
     SpiceDouble lt;
-    // spkpos_c("SUN", et, "MOON_ME", "NONE", "MOON", ptarg, &lt);
-    // spkpos_c("SUN", et, "ITRF93", "NONE", "MOON", ptarg, &lt);
 
-    spkpos_c("SUN", et, "IAU_EARTH", "NONE", "MOON", ptarg, &lt);
+    const char* bodies[] { "SUN", "MERCURY", "VENUS", "MOON", "EARTH", "MARS BARYCENTER", "JUPITER BARYCENTER",
+            "SATURN BARYCENTER", "URANUS BARYCENTER", "NEPTUNE BARYCENTER", "PLUTO BARYCENTER", "" };
 
-    cout << (ptarg[0] * 1000) << std::endl;
-    cout << (ptarg[1] * 1000) << std::endl;
-    cout << (ptarg[2] * 1000) << std::endl;
+    float SCALE = 0.0000001;
+
+    cout << "{";
+    for (int i = 0; ; i++) {
+        if (strlen(bodies[i]) == 0) break;
+        spkpos_c("SUN", et, "IAU_SUN", "NONE", bodies[i], ptarg, &lt);
+
+        float x = ptarg[0] * SCALE;
+        float y = ptarg[1] * SCALE;
+        float z = ptarg[2] * SCALE;
+
+        char buffer[1024];
+        snprintf(buffer, sizeof(buffer), "\"%s\": { x: %f, y: %f, z: %f }", bodies[i], x, y, z);
+        bool first = (i == 0);
+        cout << (first ? "" : ",") << endl << "    " << buffer;
+    }
+    cout << endl << "}" << endl;
 
     kclear_c();
 }
