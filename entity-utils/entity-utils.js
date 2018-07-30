8b71ac4e11ea4b11aa8000f19a35534a459c8e1d
Seth Alves <seth.alves@gmail.com>
2018-07-29 17:10:59 -0700

    sync

--- entity-utils/entity-utils.js
+++ entity-utils/entity-utils.js
@@ -371,12 +371,46 @@ function getConnectedEntityIDs(origID) {
 }
 
 
+// https://stackoverflow.com/questions/201183/how-to-determine-equality-for-two-javascript-objects#16788517
+function objectsAlmostEqual(x, y) {
+    // test for deep equality, but allow some slack in numbers
+    if (x === null || x === undefined || y === null || y === undefined) { return x === y; }
+    // after this just checking type of one would be enough
+    if (x.constructor !== y.constructor) { return false; }
+    // if they are functions, they should exactly refer to same one (because of closures)
+    if (x instanceof Function) { return x === y; }
+    // if they are regexps, they should exactly refer to same one (it is hard to better equality check on current ES)
+    if (x instanceof RegExp) { return x === y; }
+
+    if (typeof x == typeof 5 && typeof y == typeof 5) {
+        return Math.abs(x - y) < 0.003;
+    }
+
+    if (x === y || x.valueOf() === y.valueOf()) { return true; }
+    if (Array.isArray(x) && x.length !== y.length) { return false; }
+
+    // if they are dates, they must had equal valueOf
+    if (x instanceof Date) { return false; }
+
+    // if they are strictly equal, they both need to be object at least
+    if (!(x instanceof Object)) { return false; }
+    if (!(y instanceof Object)) { return false; }
+
+    // recursive object equality check
+    var p = Object.keys(x);
+    return Object.keys(y).every(function (i) { return p.indexOf(i) !== -1; }) &&
+        p.every(function (i) { return objectsAlmostEqual(x[i], y[i]); });
+}
+
+
 function propertySetsAreSimilar(propsA, propsB) {
     // propsA and propsB should both be the sort of thing returned by entitiesIDsToProperties:
     // { Entities: [ ... ], Actions: [ ... ] }
     // TODO -- examine the parent/child and neighbor relationships and the actions
 
     if (propsA.Entities.length != propsB.Entities.length) {
+        print("QQQQ propsA.Entities.length != propsB.Entities.length -- " +
+              propsA.Entities.length + " " + propsB.Entities.length);
         return false;
     }
 
@@ -418,13 +452,34 @@ function propertySetsAreSimilar(propsA, propsB) {
     scrubPropsList(entityPropsACopy);
     scrubPropsList(entityPropsBCopy);
 
-    // print("QQQQ entityPropsACopy = " + JSON.stringify(entityPropsACopy));
-    // print("QQQQ entityPropsBCopy = " + JSON.stringify(entityPropsBCopy));
+    print("QQQQ entityPropsACopy = " + JSON.stringify(entityPropsACopy));
+    print("QQQQ entityPropsBCopy = " + JSON.stringify(entityPropsBCopy));
 
-    var result = (JSON.stringify(entityPropsACopy) == JSON.stringify(entityPropsBCopy));
+    // var result = (JSON.stringify(entityPropsACopy) == JSON.stringify(entityPropsBCopy));
+    // they are sorted by IDs, and these have changed, so the above line can fail.
+    for (var k = 0; k < entityPropsACopy.length; k++) {
+        var aCopy = entityPropsACopy[k];
+        var foundMatch = false;
+        for (var m = 0; m < entityPropsBCopy.length; m++) {
+            var bCopy = entityPropsBCopy[m];
+            // if (JSON.stringify(propA) == JSON.stringify(propB)) {
+            if (objectsAlmostEqual(aCopy, bCopy)) {
+                foundMatch = true;
+                if (m === 0) {
+                    entityPropsBCopy.shift();
+                }
+                break;
+            }
+        }
+        if (!foundMatch) {
+            print("QQQQ no match for " + JSON.stringify(aCopy));
+            print("QQQQ similar result = false");
+            return false;
+        }
+    }
 
-    // print("QQQQ similar result = " + result);
-    return result;
+    print("QQQQ similar result = true");
+    return true;
 }
 
 
