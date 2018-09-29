5144b06668fa04f351f3d20867875d236b38145c
Seth Alves <seth.alves@gmail.com>
2018-09-29 14:03:45 -0700

    check for far-trigger

--- misc/touch-test.js
+++ misc/touch-test.js
@@ -3,37 +3,47 @@
     this.preload = function (entityID) {
         this.entityID = entityID;
         this.turnOff();
-    }
+    };
 
     this.turnOn = function () {
         Entities.editEntity(this.entityID, { locked: false });
         Entities.editEntity(this.entityID, { color: { blue: 0, green: 255, red: 0 }});
         Entities.editEntity(this.entityID, { locked: true });
-    }
+    };
 
     this.turnOff = function () {
         Entities.editEntity(this.entityID, { locked: false });
         Entities.editEntity(this.entityID, { color: { blue: 0, green: 0, red: 255 }});
         Entities.editEntity(this.entityID, { locked: true });
-    }
+    };
 
     this.startNearTrigger = function (entityID) {
         this.entityID = entityID;
         this.turnOn();
-    }
+    };
 
     this.stopNearTrigger = function (entityID) {
         this.entityID = entityID;
         this.turnOff();
-    }
+    };
 
     this.clickDownOnEntity = function (entityID, mouseEvent) {
         this.entityID = entityID;
         this.turnOn();
-    }
+    };
 
     this.clickReleaseOnEntity = function (entityID, mouseEvent) {
         this.entityID = entityID;
         this.turnOff();
-    }
+    };
+
+    this.startFarTrigger = function (entityID) {
+        this.entityID = entityID;
+        this.turnOn();
+    };
+
+    this.stopFarTrigger = function (entityID) {
+        this.entityID = entityID;
+        this.turnOff();
+    };
 })
