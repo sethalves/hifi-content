b93a7c79023c8ffe26cfced2a356abf8ee974fce
Seth Alves <seth.alves@gmail.com>
2019-09-25 10:53:55 -0700

    sync

--- redirected-walking-test/redirected-walking-test.js
+++ redirected-walking-test/redirected-walking-test.js
@@ -1,5 +1,5 @@
 
-/* global Script, Controller */
+/* global Script, Controller, MyAvatar, Quat */
 /* jshint loopfunc:true */
 
 (function() {
@@ -13,8 +13,20 @@
     var leftTurn = Quat.fromPitchYawRollDegrees(0, 3.0, 0);
     var closedValue = 0.7;
 
+    var avatarOrientation = null;
+
+    // avoid slow call into c++ Quat.multiply
+    function QuatMultiply(a, b) {
+        return {
+            w: a.w * b.w - a.x * b.x - a.y * b.y - a.z * b.z,  // 1
+            x: a.w * b.x + a.x * b.w + a.y * b.z - a.z * b.y,  // i
+            y: a.w * b.y - a.x * b.z + a.y * b.w + a.z * b.x,  // j
+            z: a.w * b.z + a.x * b.y - a.y * b.x + a.z * b.w   // k
+        };
+    }
+
     function go() {
-        MyAvatar.orientation = Quat.multiply(MyAvatar.orientation, leftTurn);
+        MyAvatar.orientation = QuatMultiply(avatarOrientation, leftTurn);
     }
 
     function leftBlinkChanged(value) {
@@ -27,6 +39,7 @@
             }
         } else {
             leftClosed = false;
+            avatarOrientation = MyAvatar.orientation;
         }
     }
 
@@ -40,8 +53,38 @@
             }
         } else {
             rightClosed = false;
+            avatarOrientation = MyAvatar.orientation;
         }
     }
+
+    // function leftBlinkChanged(value) {
+    //     if (value > closedValue) {
+    //         if (!leftClosed) {
+    //             leftClosed = true;
+    //             if (!rightClosed) {
+    //                 go();
+    //             }
+    //         }
+    //     } else {
+    //         leftClosed = false;
+    //         avatarOrientation = MyAvatar.orientation;
+    //     }
+    // }
+
+    // function rightBlinkChanged(value) {
+    //     if (value > closedValue) {
+    //         if (!rightClosed) {
+    //             rightClosed = true;
+    //             if (!leftClosed) {
+    //                 go();
+    //             }
+    //         }
+    //     } else {
+    //         rightClosed = false;
+    //         avatarOrientation = MyAvatar.orientation;
+    //     }
+    // }
+
 
     function cleanUp() {
         inputMapping.disable();
